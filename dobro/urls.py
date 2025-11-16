4928cd86952ce9bfb977ff42a0c2d84a1f9fa712
MrZareHG <32684873+MrZareHG@users.noreply.github.com>
2025-11-16 17:08:14 +0300

    Add files via upload

--- dobro/urls.py
+++ dobro/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from content.views import home
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('organizations/', include('organizations.urls')),
     path('accounts/', include('accounts.urls')),
